9d70153d5d46e19a9e18a1ffdf66f596d2a4084a
Baptiste Meurant <baptiste.meurant@gmail.com>
2015-11-02 23:23:56 +0100

    Add CSS stylesheet

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,4 +1,5 @@
 'use strict';
+const tildeImporter = require('node-sass-tilde-importer');
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
@@ -7,10 +8,13 @@ module.exports = function(defaults) {
     'ember-bootstrap': {
       'bootstrapVersion': 4,
       'importBootstrapFont': false,
-      'importBootstrapCSS': false
+      'importBootstrapCSS': true
     },
-    'ember-font-awesome': {
-      useScss: true
+    sassOptions: {
+      importer: tildeImporter,
+      includePaths: [
+        'node_modules/@fortawesome/fontawesome-free/scss'
+      ]
     }
   });
 
