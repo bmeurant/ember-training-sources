3f809a1954806948a25e1d80c934e0d5c5ca948d
Baptiste Meurant <baptiste.meurant@gmail.com>
2016-04-04 21:57:00 +0200

    Components - 5 - lifecyle

--- app/components/image-cover.js
+++ app/components/image-cover.js
@@ -1,11 +1,34 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import $ from 'jquery';
 
 export default Component.extend({
   tagName: 'img',
   classNames: 'cover',
   attributeBindings: 'src',
   src: computed('name', function () {
-    return `/assets/images/comics/covers/${this.get('name')}.jpg`;
-  })
+    return this.getImagePath(this.get('name'));
+  }),
+  currentNode: computed('elementId', function() {
+    return $('#' + this.get('elementId'));
+  }),
+
+  getImagePath(name) {
+    return `/assets/images/comics/covers/${name}.jpg`;
+  },
+
+  didInsertElement(...args) {
+    this._super(...args);
+    this.get('currentNode').on('error', () => {
+      return this.onError();
+    });
+  },
+
+  willDestroyElement(){
+    this.get('currentNode').off('error');
+  },
+
+  onError() {
+    this.get('currentNode').attr('src', this.getImagePath('default'));
+  }
 });
