6074b7433a57f09d1b9ca29f9dbda62dc431f398
Baptiste Meurant <baptiste.meurant@gmail.com>
2016-04-20 17:21:17 +0200

    Ember Data 3 - Saving & states

--- app/controllers/comic/edit.js
+++ app/controllers/comic/edit.js
@@ -3,16 +3,6 @@ import Controller from '@ember/controller';
 export default Controller.extend({
 
   actions: {
-    save() {
-      this.set('hasUserSavedOrCancel', true);
-      return true;
-    },
-
-    cancel() {
-      this.set('hasUserSavedOrCancel', true);
-      return true;
-    },
-
     favorize() {
       this.send('onFavorize');
     }
